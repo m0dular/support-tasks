88a105e595c5da262d9ea3e7b1072da644e0d21e
Adrian Parreiras Horta <sensei.loafage@gmail.com>
2023-10-30 13:26:02 -0700

    reject

--- tasks/st0317a_clean_cert.rb
+++ tasks/st0317a_clean_cert.rb
@@ -65,4 +65,4 @@ end
 
 puts results.to_json
 
-exit(results.values.select{|v| v != deprecation_msg}.all? { |v| v[:result] == 'Certificate removed' }) ? 0 : 1
+exit(results.values.reject { |v| v == deprecation_msg }.all? { |v| v[:result] == 'Certificate removed' }) ? 0 : 1
